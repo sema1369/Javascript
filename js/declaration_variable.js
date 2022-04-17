ccdf3e616cd52b16c4111c5bc81eca00d5ddfaec
Sema1369 <sema136910@gmail.com>
2022-04-17 15:40:52 +0200

    Update javascript

---
+++ js/declaration_variable.js
@@ -0,0 +1,15 @@
+// FICHIER JS EXTERNE - DECLARATION DE VARIABLE - SEMA
+
+// 1) DECLARATION D'UNE VARIABLE.
+
+let nombreDeChats = 2; 
+// let nombreDeChats = Je déclare ma variable et lui donne un nom.
+// Le signe égal '=' est un opérateur d'affectation. 
+// J'initialise ma variable en lui affectant un nombre '2'. 
+
+let nombreDeChiens = 4; 
+
+// 2) MODIFICATION DE LA VALEUR D'UNE VARIABLE. 
+
+nombreDeChats = 4; 
+// Je réaffecte une autre valeur à ma variable. 
