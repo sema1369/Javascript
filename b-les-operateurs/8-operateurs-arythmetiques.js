ea423ed6862f14fdc582d25dcf5556c2843ec6e8
Sema1369 <sema136910@gmail.com>
2022-05-03 16:26:27 +0200

    Update operateur

---
+++ b-les-operateurs/8-operateurs-arythmetiques.js
@@ -0,0 +1,23 @@
+// 8 - LES OPERATEURS ARYTHMETIQUES - SEMA
+
+let x = 10;
+let y = 3;
+
+console.log(x + y); /* Addition : le résultat est 13*/
+console.log(x - y); /* Soustraction : le résultat est 7 */
+console.log(x * y); /* Multiplication : le résultat est 30*/
+console.log(x / y); /* Division : le résultat est 3.3333333 */
+console.log(x % y); /* Modulo : le résultat est 1*/
+console.log(x ** y); /* Exponentielle : le résultat est 1000 */
+
+/* ======================================== A- INCREMENTATION ======================================= */
+
+console.log(++x); /* On incrémente de 1 : le résultat est 11  */
+
+console.log(x++); 
+console.log(x);
+/* On incrémente encore de 1 : le résultat est 12 */
+
+/* ======================================== B- DECREMENTATION ======================================= */
+
+console.log(--y); /* On décrémente de 1 : le résultat est 2 */
---
+++ b-les-operateurs/8-operateurs-d'affectations.js
@@ -0,0 +1,20 @@
+// 8 - LES OPERATEURS ARYTHMETIQUES - SEMA
+
+let x = 10;
+let y = 3;
+
+console.log(y += 10); // Additionne puis affecte le résultat 
+// y = y + 10; donne le meme résultat 
+
+console.log(y -= 3);  // Soustrait puis affecte le résultat 
+// y = y - 3; donne le meme résultat 
+
+
+console.log(x *= 5); // Multiplie puis affecte le résultat 
+// x = x * 5; donne le meme résultat
+
+console.log(x /= 2);  // Divise puis affecte le résultat
+// x = x / 2; donne le meme résultat
+
+console.log(x %= 2);  // Calcul le modulo puis affecte le résultat 
+// x = x % 2; donne le meme résultat. 
