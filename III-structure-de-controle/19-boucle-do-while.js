cc41a21b2cbcc28cd45affb19dbb42edb322ce37
Sema1369 <sema136910@gmail.com>
2022-05-11 11:41:47 +0200

    Update 19-boucle-do-while.js

--- III-structure-de-controle/19-boucle-do-while.js
+++ III-structure-de-controle/19-boucle-do-while.js
@@ -1,15 +1,14 @@
 // 19 - STRUCTURE DE CONTROLE - BOUCLE DO WHILE - SEMA
 
 let while1 = 6;
-// 1 - J'initialise ma variable a 6
+// 1 - J'initialise ma variable à 6
 
 while (while1 < 5) {
 // 2 - Je pose une condition : Si i est inférieur à 5 alors execute le bloc d'instruction suivant :
     console.log('Une boucle while', while1);
     while1++;
-    // 3 - Le bloc  d'instruction s'execute s'incremente de 1 a chaque fois jusqu'à arriver a 5
+    // 3 - Le bloc  d'instruction s'execute et s'incremente de 1 à chaque fois jusqu'à arriver à 5.
 }
-
 // Dans cet exemple le résultat ne s'affiche pas dans la console car la condition est false dès le depart donc la boucle s'arrete directement et ne va pas plus loin.
 
 let while2 = 6; 
