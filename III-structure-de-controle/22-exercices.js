f85ee1cd77c3120833b9a9fb3aa76104d0bf3226
Sema1369 <sema136910@gmail.com>
2022-06-02 14:17:44 +0200

    exercice finished

--- III-structure-de-controle/22-exercices.js
+++ III-structure-de-controle/22-exercices.js
@@ -55,3 +55,93 @@ function fizzBuzz(input){
 
     return input;  
 }
+
+/* ======================================== EXERCICE 4 ======================================= */
+
+// Ecrire tout ca dans une fonction : 
+// La vitesse limite est de 70 km/h 
+// Si la vitesse limite est dépassé de 5km/h rajouter 1 points
+// Si Arrivé à 12 points afficher "permis suspendu" 
+
+checkSpeed(130);
+
+function checkSpeed(speed) {
+    const speedLimit = 70;
+    // Je déclare une variable, je la nomme speedLimit, je lui affecte une valeur "70". 
+
+    const kmhPerPoint = 5; 
+    // Je déclare une variable, je la nomme speedLimit, je lui affecte une valeur "5".
+    
+    if (speed < speedLimit + kmhPerPoint) {
+    // Si ma vitesse est inférieur à 70 + 5... 
+        console.log('ok');
+        // ... Tu m'affiche dans la log 'ok'. 
+    } else {
+        let points = Math.floor((speed - speedLimit) / kmhPerPoint);
+        // Sert à calculer les points
+        if (points >= 12) {
+        // Si on arrive ou l'on dépasse 12 points...
+            console.log('Permis suspendu');
+            // Affiche dans la log 'permis suspendu'
+        } else {
+            console.log('Points', points);
+            // Sinon affiche les points
+        }
+    }
+}
+
+/* ======================================== EXERCICE 5 ======================================= */
+
+// Ecrire une fonction :
+// Afficher dans la log 10 chiffre
+// Afficher Pair pour les chiffre pair et impair pour les chiffres impairs
+
+showNumbers(10);
+
+function showNumbers(limit) {
+    for (let i = 0; i <= limit; i++) {
+    // J'initialise ma variable i à 0, Si i est inferieur ou = à limit, tu rajoutes 1 à chaque boucle
+        const message = (i % 2 === 0) ? 'EVEN' : 'ODD';
+        // Je declare une variable "message", j'utilise un l'operateur ternaire.
+        console.log(i, message);
+        // J'affiche dans la log le resultat. 
+    }
+}
+
+/* ======================================== EXERCICE 6 ======================================= */
+
+const movie = {
+    title: 'a',
+    releaseYear: 2008,
+    rating: 4.5, 
+    director: 'b'
+}; 
+
+// Afficher à l'aide d'une fonction les propriétés qui contiennent une string
+
+showProperties(movie);
+
+function showProperties(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] === 'string') {
+            console.log(key, obj[key]);
+        }
+    }
+}
+
+/* ======================================== EXERCICE 7 ======================================= */
+
+// Ecrire une fonction en affichant des etoiles 
+
+showStars(1);
+
+function showStars(rows) {
+    for (let row = 1; row <= rows; row++) {
+        let pattern = '';
+        for (let i = 0; i < row; i++) {
+            pattern += '*';
+            console.log(pattern);
+        }
+    }
+}
+
